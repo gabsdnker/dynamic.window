8f879f5d33c62ca43c75536586dc9257b17907f7
marcos <marcosap103@gmail.com>
2023-05-09 19:57:48 -0300

    primeiro commit via terminal

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -13,7 +13,9 @@ class User(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+
+    dados = db.session.query(User).all() 
+    return render_template('index.html', listagem = dados)
 
 @app.route('/cadastrar', methods=['POST'])
 def cadastrar():
@@ -22,10 +24,25 @@ def cadastrar():
     user = User(name=name, idade=idade)
     db.session.add(user)
     db.session.commit()
-    return 'Cadastro realizado com sucesso!'
+    return redirect("/")
+    #return 'Cadastro realizado com sucesso!'
 
 with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+'''
+a) ver como ler os nomes da tabela que existem em um banco de dados sqlite
+https://www.geeksforgeeks.org/how-to-list-tables-using-sqlite3-in-python/
+b) listar os nomes das tabelas
+c) ao clicar em um nome de tabela..... thcaram....
+d) verifica quais campos existem na tabela
+https://stackoverflow.com/questions/7831371/is-there-a-way-to-get-a-list-of-column-names-in-sqlite
+e) faz um "for" nesses campos
+f) cria uma caixa de texto (input) para cada campo
+assim eu montei o formulário genérico
+
+'''
